33135241088f91f91901332dc3d51dcca8cc1cde
Annida-D <nidanisadika@gmail.com>
2024-09-19 09:56:05 +0700

    upload
    upload

---
+++ README.md
@@ -0,0 +1,6 @@
+"# Jadwal-FrameworkNID" 
+"# Jadwal-FrameworkNID" 
+"# JadwalAnnida" 
+"# Framework_AnnidaD" 
+"# Framework_AnnidaD" 
+"# annida_TI3c" 
---
+++ application/controllers/Beranda.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+  * 
+  */
+ class Beranda extends CI_Controller
+ {
+ 	public function index()
+ 	{
+ 		 $data['judul']= 'Halaman Beranda';
+ 		$this->load->view('templates/header',$data);
+ 		$this->load->view('Beranda/index',$data);
+ 		$this->load->view('templates/footer');
+
+
+
+
+ 	}
+ }
+  ?>
